22423291e0496e15fdee57d0fc082511d5b1a1b2
William Law <wlaw@stanford.edu>
2018-07-05 22:15:06 -0700

    Hopefully fixed issue with slurmdbd starting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,17 +22,6 @@ fi
 if [ "$1" = "slurmdbd" ]
 then
     echo "---> Starting the Slurm Database Daemon (slurmdbd) ..."
-
-    {
-        . /etc/slurm/slurmdbd.conf
-        until echo "SELECT 1" | mysql -h $StorageHost -u$StorageUser -p$StoragePass 2>&1 > /dev/null
-        do
-            echo "-- Waiting for database to become active ..."
-            sleep 2
-        done
-    }
-    echo "-- Database is now active ..."
-
     exec gosu slurm /usr/sbin/slurmdbd -Dvvv
 fi
 
